1a5b90ad0deb8253cf9f76e8bc9019c4a3ec3649
Bernard Historillo <bernardhistorillo1@gmail.com>
2024-05-04 09:40:30 +0800

    Add deployer script

---
+++ deployer.sh
@@ -0,0 +1,29 @@
+# Path to your project directory
+PROJECT_DIR="/var/www/laravel-boilerplate"
+
+# Log file path
+LOG_FILE="$(PROJECT_DIR)/storage/logs/deployment.log"
+
+# Navigate to the project directory
+cd $PROJECT_DIR
+
+git stash push --include-untracked
+git stash drop
+git pull origin main
+
+sudo chown -R $USER:www-data storage
+sudo chown -R $USER:www-data bootstrap/cache
+
+chmod -R 775 storage
+chmod -R 775 bootstrap/cache
+
+/usr/bin/php8.1 /usr/local/bin/composer install --no-interaction
+
+/usr/bin/php8.1 artisan migrate --force
+/usr/bin/php8.1 artisan config:cache
+/usr/bin/php8.1 artisan view:cache
+
+npm run build
+
+# Log deployment
+echo "Deployment successful at $(date)" >> $LOG_FILE
