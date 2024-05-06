47d6ef61349d523f6452bf638e06989d039b61a6
Bernard Historillo <bernardhistorillo1@gmail.com>
2024-05-06 20:14:31 +0800

    Update deployer script

--- deployer.sh
+++ deployer.sh
@@ -17,7 +17,7 @@ sudo chown -R $USER:www-data bootstrap/cache
 chmod -R 775 storage
 chmod -R 775 bootstrap/cache
 
-/usr/bin/php8.1 /usr/local/bin/composer dump-autoload
+/usr/bin/php8.1 /usr/local/bin/composer dump-autoload --no-interaction
 /usr/bin/php8.1 /usr/local/bin/composer install --no-interaction
 
 /usr/bin/php8.1 artisan cache:clear
