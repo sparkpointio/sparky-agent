cd7c4af57c09926cc60f03cb05af93cb56483f23
Bernard Historillo <bernardhistorillo1@gmail.com>
2024-05-09 22:58:28 +0800

    Update email verification

--- resources/views/layouts/email.blade.php
+++ resources/views/layouts/email.blade.php
@@ -26,7 +26,7 @@
                             <hr class="my-4 "/>
                         </div>
 
-                        <p class="mb-0"><img src="{{ config('app.prod_url') . '/img/home/logo.png' }}" width="20" alt="{{ config('app.name') }}" style="margin-top:-4px" />&nbsp;&nbsp; &copy; {{ date('Y') }} {{ config('app.name') }}. All rights reserved.</p>
+                        <p class="mb-0"><img src="{{ config('app.email_assets_url') . '/img/home/logo.png' }}" width="20" alt="{{ config('app.name') }}" style="margin-top:-4px" />&nbsp;&nbsp; &copy; {{ date('Y') }} {{ config('app.name') }}. All rights reserved.</p>
                     </div>
                 </div>
             </div>
