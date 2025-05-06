93c381fe1380687d41ef2aea31631f5e3b7fa981
Bernard Historillo <bernardhistorillo1@gmail.com>
2025-05-07 00:01:42 +0800

    Update deployer script

--- deployer.sh
+++ deployer.sh
@@ -9,7 +9,7 @@ cd $PROJECT_DIR
 
 git stash push --include-untracked
 git stash drop
-git pull origin master
+git pull origin develop
 
 sudo chown -R $USER:www-data storage
 sudo chown -R $USER:www-data bootstrap/cache
